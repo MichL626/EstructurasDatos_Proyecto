36e0e02413d0ed1e3880805bc9b55fa95e2586a5
Michell Bonilla Leitón <mbonillal@ucenfotec.ac.cr>
2026-02-15 18:11:17 -0600

    Clase Lista Enlazada y NodoProducto

--- NodoProducto.java
+++ NodoProducto.java
@@ -1,2 +1,24 @@
 public class NodoProducto {
+    private Producto dato;
+    private NodoProducto siguiente;
+
+    public NodoProducto(Producto dato) {
+        this.dato = dato;
+        siguiente = null;
+    }
+
+    public Producto getDato() {return dato;}
+    public void setDato(Producto dato) {this.dato = dato;}
+
+    public NodoProducto getSiguiente() {return siguiente;}
+    public void setSiguiente(NodoProducto siguiente) {this.siguiente = siguiente;}
+
+    @Override
+    public String toString() {
+        return "NodoProducto{" +
+                "dato=" + dato +
+                ", siguiente=" + siguiente +
+                '}';
+    }
+
 }
