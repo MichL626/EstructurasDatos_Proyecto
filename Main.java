eff94e0ad2a422846360fe2919f397be7b2663dc
Raquel Bueno <rbuenon@ucenfotec.ac.cr>
2026-02-15 21:00:57 -0500

    Actualización de main

--- Main.java
+++ Main.java
@@ -3,23 +3,23 @@ import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class Main {
-    
+
     public static void main(String[] args) {
 
         // CRUD -> Create, read, update, delete.
 
         ListaProducto listaProductos = new ListaProducto();
 
-        listaProductos.insertarProductoFinal("001", "Pan", (byte) 20, (double)500, "Pan", 2026/05/01, "Imagen 1");
-        listaProductos.insertarProductoFinal("002", "Pan con queso", (byte) 20, (double)550, "Pan", 2026-05-01, "Imagen 2");
-        listaProductos.insertarProductoFinal("003", "Pastel de pollo", (byte) 5, (double)700, "Repostería", 2026-05-04, "Imagen 3");
-        listaProductos.insertarProductoFinal("004", "Pastel de carne", (byte) 5, (double)700, "Repostería", 2026-05-02, "Imagen 4");
-        listaProductos.insertarProductoFinal("005", "Flauta de Guayaba", (byte) 5, (double)700, "Repostería", 2026-05-03, "Imagen 5");
-        listaProductos.insertarProductoFinal("006", "Pañelo", (byte) 5, (double)700, "Repostería", 2026-05-04, "Imagen 6");
-        listaProductos.insertarProductoFinal("007", "Leche", (byte) 5, (double)700, "Abarrotes", 2026-05-15, "Imagen 7");
-        listaProductos.insertarProductoFinal("008", "Mantequilla", (byte) 5, (double)700, "Abarrotes", 2026-05-15, "Imagen 8");
-        listaProductos.insertarProductoFinal("009", "Paté", (byte) 5, (double)700, "Abarrotes", 2026-05-15, "Imagen 9");
-        listaProductos.insertarProductoFinal("010", "Natilla", (byte) 5, (double)700, "Abarrotes", 2026-05-15, "Imagen 10");
+        listaProductos.insertarProductoFinal("001", "Pan", (byte) 20, (double)500, "Pan", LocalDate.parse("2026-05-01"), "Imagen 1");
+        listaProductos.insertarProductoFinal("002", "Pan con queso", (byte) 20, (double)550, "Pan", LocalDate.parse("2026-05-01"), "Imagen 2");
+        listaProductos.insertarProductoFinal("003", "Pastel de pollo", (byte) 5, (double)700, "Repostería", LocalDate.parse("2026-05-04"), "Imagen 3");
+        listaProductos.insertarProductoFinal("004", "Pastel de carne", (byte) 5, (double)700, "Repostería", LocalDate.parse("2026-05-05"), "Imagen 4");
+        listaProductos.insertarProductoFinal("005", "Flauta de Guayaba", (byte) 5, (double)700, "Repostería", LocalDate.parse("2026-05-03"), "Imagen 5");
+        listaProductos.insertarProductoFinal("006", "Pañelo", (byte) 5, (double)700, "Repostería", LocalDate.parse("2026-05-02"), "Imagen 6");
+        listaProductos.insertarProductoFinal("007", "Leche", (byte) 5, (double)700, "Abarrotes", LocalDate.parse("2026-05-15"), "Imagen 7");
+        listaProductos.insertarProductoFinal("008", "Mantequilla", (byte) 5, (double)700, "Abarrotes", LocalDate.parse("2026-05-15"), "Imagen 8");
+        listaProductos.insertarProductoFinal("009", "Paté", (byte) 5, (double)700, "Abarrotes", LocalDate.parse("2026-05-15"), "Imagen 9");
+        listaProductos.insertarProductoFinal("010", "Natilla", (byte) 5, (double)700, "Abarrotes", LocalDate.parse("2026-05-15"), "Imagen 10");
 
         listaProductos.mostrarLista();
 
