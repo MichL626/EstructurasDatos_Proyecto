608b5762832b6cdfb99bf2f9b3c240176db9046f
Raquel Bueno <rbuenon@ucenfotec.ac.cr>
2026-02-15 20:00:58 -0500

    ListaProducto terminada

--- Main.java
+++ Main.java
@@ -3,17 +3,6 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class Main {
-<<<<<<< HEAD
 
-    private static Scanner sc = new Scanner(System.in);
-    private static final DateTimeFormatter FORMATO = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-    public static void main(String[] args) {
-        ListaProducto lista = new ListaProducto();
-
-
-    }
 }
-=======
-} //hola!
->>>>>>> 9861d5fdc816456f9257534a03c04efbeda30d4b
+
